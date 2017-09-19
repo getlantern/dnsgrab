629173d928f2b769f3490461c6dd454bbc0ba92f
Ox Cart <ox.to.a.cart@gmail.com>
2017-09-19 16:41:24 -0500

    Sending queries upstream when necessary

--- dnsgrab.go
+++ dnsgrab.go
@@ -1,9 +1,10 @@
-package main
+package dnsgrab
 
 import (
 	"encoding/binary"
 	"net"
 	"sync"
+	"sync/atomic"
 	"time"
 
 	"github.com/getlantern/golog"
@@ -17,77 +18,119 @@ const (
 var (
 	log = golog.LoggerFor("dnsgrab")
 
-	bigEndian = binary.BigEndian
-
-	// TODO: clear out domains after TTL
-	domains = make(map[uint32]string, 1000)
-	mx      sync.RWMutex
+	endianness = binary.BigEndian
 )
 
-func main() {
-	go debugQueries()
+type Server interface {
+	Serve() error
 
-	addr, err := net.ResolveUDPAddr("udp", ":55899")
-	if err != nil {
-		log.Fatal(err)
+	Close() error
+
+	ReverseLookup(ip net.IP) string
+}
+
+type server struct {
+	defaultDNSServer string
+	conn             *net.UDPConn
+	client           *dns.Client
+	idx              uint32
+	domains          map[uint32]string
+	mx               sync.RWMutex
+}
+
+func Listen(listenAddr string, defaultDNSServer string) (Server, error) {
+	s := &server{
+		defaultDNSServer: defaultDNSServer,
+		domains:          make(map[uint32]string, 1000),
+		client: &dns.Client{
+			DialTimeout: 2 * time.Second,
+			ReadTimeout: 2 * time.Second,
+		},
 	}
-	in, err := net.ListenUDP("udp", addr)
+
+	// TODO: clear out domains after TTL
+	addr, err := net.ResolveUDPAddr("udp", listenAddr)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
-	defer in.Close()
-	log.Debugf("Listening at: %v", in.LocalAddr())
+	s.conn, err = net.ListenUDP("udp", addr)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Debugf("Listening at: %v", s.conn.LocalAddr())
+	return s, nil
+}
+
+func (s *server) Serve() error {
 
 	b := make([]byte, maxUDPPacketSize)
-	idx := uint32(0)
 	for {
-		n, remoteAddr, err := in.ReadFromUDP(b)
+		n, remoteAddr, err := s.conn.ReadFromUDP(b)
 		if err != nil {
-			log.Fatal(err)
+			log.Error(err)
+			continue
 		}
 		msgIn := &dns.Msg{}
 		msgIn.Unpack(b[:n])
-		if len(msgIn.Question) == 0 {
-			// TODO: forward the message upstream
-		}
-		msgOut := &dns.Msg{}
-		msgOut.Response = true
-		msgOut.Id = msgIn.Id
-		msgOut.Question = msgIn.Question
-		for _, question := range msgIn.Question {
-			if question.Qclass == dns.ClassINET && question.Qtype == dns.TypeA {
-				answer := &dns.A{}
-				// Short TTL should be fine since these DNS lookups are local and should be quite cheap
-				answer.Hdr = dns.RR_Header{Name: question.Name, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 1}
-				fakeIP := make(net.IP, 4)
-				bigEndian.PutUint32(fakeIP, idx)
-				answer.A = fakeIP
-				msgOut.Answer = append(msgOut.Answer, answer)
-				// Remember the query
-				mx.Lock()
-				domains[idx] = question.Name
-				mx.Unlock()
-				idx++
-			} else {
-				// TODO: resolve upstream
-			}
-		}
-
-		bo, err := msgOut.Pack()
-		if err != nil {
-			log.Fatal(err)
-		}
-		in.WriteToUDP(bo, remoteAddr)
+		go s.handle(remoteAddr, msgIn)
 	}
 }
 
-func debugQueries() {
-	for {
-		time.Sleep(1 * time.Second)
-		mx.RLock()
-		for id, name := range domains {
-			log.Debugf("%d -> %v", id, name)
-		}
-		mx.RUnlock()
+func (s *server) Close() error {
+	return s.conn.Close()
+}
+
+func (s *server) ReverseLookup(ip net.IP) string {
+	ipInt := endianness.Uint32(ip)
+	s.mx.RLock()
+	result := s.domains[ipInt]
+	s.mx.RUnlock()
+	return result
+}
+
+func (s *server) handle(remoteAddr *net.UDPAddr, msgIn *dns.Msg) {
+	if len(msgIn.Question) == 0 {
+		// TODO: forward the message upstream
 	}
+
+	msgOut := &dns.Msg{}
+	msgOut.Response = true
+	msgOut.Id = msgIn.Id
+	msgOut.Question = msgIn.Question
+	var unansweredQuestions []dns.Question
+	for _, question := range msgIn.Question {
+		if question.Qclass == dns.ClassINET && question.Qtype == dns.TypeA && question.Name == "www.google.com" {
+			answer := &dns.A{}
+			// Short TTL should be fine since these DNS lookups are local and should be quite cheap
+			answer.Hdr = dns.RR_Header{Name: question.Name, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 1}
+			fakeIP := make(net.IP, 4)
+			idx := atomic.AddUint32(&s.idx, 1)
+			endianness.PutUint32(fakeIP, idx)
+			answer.A = fakeIP
+			msgOut.Answer = append(msgOut.Answer, answer)
+			// Remember the query
+			s.mx.Lock()
+			s.domains[idx] = question.Name
+			s.mx.Unlock()
+		} else {
+			unansweredQuestions = append(unansweredQuestions, question)
+		}
+	}
+
+	if len(unansweredQuestions) > 0 {
+		msgIn.Question = unansweredQuestions
+		resp, _, err := s.client.Exchange(msgIn, s.defaultDNSServer)
+		if err != nil {
+			log.Error(err)
+		} else {
+			msgOut.Answer = append(msgOut.Answer, resp.Answer...)
+		}
+	}
+
+	bo, err := msgOut.Pack()
+	if err != nil {
+		log.Fatal(err)
+	}
+	s.conn.WriteToUDP(bo, remoteAddr)
 }
---
+++ dnsgrab/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/binary"
+	"fmt"
+	"time"
+
+	"github.com/getlantern/dnsgrab"
+)
+
+func main() {
+	s, err := dnsgrab.Listen(":55899", "8.8.8.8:53")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	go debugQueries(s)
+	s.Serve()
+}
+
+func debugQueries(s dnsgrab.Server) {
+	for {
+		time.Sleep(1 * time.Second)
+		ip := make([]byte, 4)
+		for id := uint32(1); id < 100000; id++ {
+			binary.BigEndian.PutUint32(ip, id)
+			name := s.ReverseLookup(ip)
+			if name == "" {
+				break
+			}
+			fmt.Printf("%d -> %v\n", id, name)
+		}
+	}
+}

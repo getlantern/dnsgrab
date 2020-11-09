ac8bdee22ef972576119da6132725dfcd67bf597
Ox Cart <ox.to.a.cart@gmail.com>
2020-11-09 06:13:12 -0600

    Added API for direct query processing

--- dnsgrab.go
+++ dnsgrab.go
@@ -43,6 +43,9 @@ type Server interface {
 	// Close closes the server's network listener.
 	Close() error
 
+	// ProcessQuery processes a DNS query and returns the response bytes.
+	ProcessQuery(b []byte) ([]byte, error)
+
 	// ReverseLookup resolves the given fake IP address into the original hostname
 	ReverseLookup(ip net.IP) string
 }
@@ -106,9 +109,7 @@ func (s *server) Serve() error {
 			log.Error(err)
 			continue
 		}
-		msgIn := &dns.Msg{}
-		msgIn.Unpack(b[:n])
-		go s.handle(remoteAddr, msgIn)
+		go s.handle(b[:n], remoteAddr)
 	}
 }
 
@@ -127,7 +128,10 @@ func (s *server) ReverseLookup(ip net.IP) string {
 	return result.Value.(string)
 }
 
-func (s *server) handle(remoteAddr *net.UDPAddr, msgIn *dns.Msg) {
+func (s *server) ProcessQuery(b []byte) ([]byte, error) {
+	msgIn := &dns.Msg{}
+	msgIn.Unpack(b)
+
 	if len(msgIn.Question) == 0 {
 		// TODO: forward the message upstream
 	}
@@ -152,16 +156,21 @@ func (s *server) handle(remoteAddr *net.UDPAddr, msgIn *dns.Msg) {
 		msgIn.Question = unansweredQuestions
 		resp, _, err := s.client.Exchange(msgIn, s.defaultDNSServer)
 		if err != nil {
-			log.Error(err)
-		} else {
-			msgOut.Answer = append(msgOut.Answer, resp.Answer...)
+			return nil, err
 		}
+		msgOut.Answer = append(msgOut.Answer, resp.Answer...)
 	}
 
-	bo, err := msgOut.Pack()
+	return msgOut.Pack()
+}
+
+func (s *server) handle(b []byte, remoteAddr *net.UDPAddr) {
+	bo, err := s.ProcessQuery(b)
 	if err != nil {
-		log.Fatal(err)
+		log.Error(err)
+		return
 	}
+
 	_, writeErr := s.conn.WriteToUDP(bo, remoteAddr)
 	if writeErr != nil {
 		log.Errorf("Error responding to DNS query: %v", writeErr)
